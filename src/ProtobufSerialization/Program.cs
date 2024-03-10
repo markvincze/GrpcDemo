a6bede313e3900ec4d83d76eff8dce480b335bce
Mark Vincze <mrk.vincze@gmail.com>
2024-03-10 10:22:44 +0100

    Initial commit

---
+++ src/ProtobufSerialization/Program.cs
@@ -0,0 +1,16 @@
+﻿using Google.Protobuf;
+using Newtonsoft.Json;
+
+Person person = new Person
+{
+    Email = "jdoe@example.com",
+    Name = "Jane Doe",
+    Id = 1
+};
+
+byte[] binary = person.ToByteArray();
+
+Person parsedPerson = Person.Parser.ParseFrom(binary);
+
+Console.WriteLine(JsonConvert.SerializeObject(person));
+Console.WriteLine(JsonConvert.SerializeObject(parsedPerson));
